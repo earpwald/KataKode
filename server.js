75be5b35c1cb86e844141e750f328c53490d3a4d
Earpwald <janderson20@hotmail.co.uk>
2014-02-25 00:09:01 +0000

    Server Setup
    Setup the installation for dependencies, creating the routing structure
    as well as setting up the logging structure and creating the server

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// Pulling in dependencies
+var log4js = require('./node_modules/log4js/lib/log4js');
+var express = require('express');
+
+log4js.configure({
+	appenders: [
+		// outputs the logging to cmd line for testing
+		{ 'type': 'console' },
+		// creates a rolling date file which will be appended with Y/M/D at midnight and a new log file will be created
+		{ 'type': 'dateFile', 'filename': 'logging/logFile.log', 'pattern': '-yyyy-MM-dd', 'alwaysIncludePattern': 'false', 'category': 'dateLog'}
+	]
+});
+
+// Create an instance of our logger and set the prefered logging type and level
+var logger = log4js.getLogger('dateLog');
+logger.setLevel('INFO');
+
+// Create the express server
+var server = express();
+
+// Configure the server to use our newly definedly logging system
+server.configure(function() {
+	server.use(log4js.connectLogger(logger));
+});
+
+logger.info('Server started now');
+
+server.get('/', function(req, res) {
+	res.send('hello world');
+});
+
+server.listen(5000);
